cd79173936a4e14a1dbc825b851dc6357498661f
paul_ <pavel.mordukhai@gmail.com>
2024-09-01 15:41:28 +0700

    KTB-05 меню консольного тренажёра new

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -11,6 +11,20 @@ data class Word(
 fun main() {
     val dictionary: MutableList<Word> = mutableListOf()
 
+    val wordsFile: File = File("words.txt")
+    wordsFile.createNewFile()
+    wordsFile.writeText("hello|привет|2\n")
+    wordsFile.appendText("dog|собака|1\n")
+    wordsFile.appendText("cat|кошка|0\n")
+    wordsFile.appendText("thank you|спасибо|")
+
+    val lines: List<String> = wordsFile.readLines()
+    for (line in lines) {
+        val splitLine = line.split("|")
+        val word = Word(splitLine[0], splitLine[1], splitLine[2].toIntOrNull() ?: 0)
+        dictionary.add(word)
+    }
+
     while (true) {
         println("""
             Меню:
@@ -24,20 +38,6 @@ fun main() {
             "0" -> break
             else -> println("Неверный ввод, введите 1, 2 или 0")
         }
-    }
-
-    val wordsFile: File = File("words.txt")
-    wordsFile.createNewFile()
-    wordsFile.writeText("hello|привет|2\n")
-    wordsFile.appendText("dog|собака|1\n")
-    wordsFile.appendText("cat|кошка|0\n")
-    wordsFile.appendText("thank you|спасибо|")
-
-    val lines: List<String> = wordsFile.readLines()
-    for (line in lines) {
-        val splitLine = line.split("|")
-        val word = Word(splitLine[0], splitLine[1], splitLine[2].toIntOrNull() ?: 0)
-        dictionary.add(word)
     }
 
     dictionary.forEach { println(it) }
