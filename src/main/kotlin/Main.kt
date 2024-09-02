774d7f15b3ca55f39a3fa0890df9b1c95f8d2f36
paul_ <pavel.mordukhai@gmail.com>
2024-09-02 17:07:12 +0700

    KTB-06 статистика выученных слов

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,10 +2,12 @@ package org.example
 
 import java.io.File
 
+const val MIN_CORRECT_ANSWERS_COUNT = 3
+
 data class Word(
     val original: String,
     val translate: String,
-    val correctAnswersCount: Int? = 0,
+    val correctAnswersCount: Int = 0,
 )
 
 fun main() {
@@ -13,9 +15,11 @@ fun main() {
 
     val wordsFile: File = File("words.txt")
     wordsFile.createNewFile()
-    wordsFile.writeText("hello|привет|2\n")
-    wordsFile.appendText("dog|собака|1\n")
-    wordsFile.appendText("cat|кошка|0\n")
+    wordsFile.writeText("hello|привет|4\n")
+    wordsFile.appendText("dog|собака|3\n")
+    wordsFile.appendText("cat|кошка|3\n")
+    wordsFile.appendText("green|зелёный|0\n")
+    wordsFile.appendText("read|читать|4\n")
     wordsFile.appendText("thank you|спасибо|")
 
     val lines: List<String> = wordsFile.readLines()
@@ -26,15 +30,26 @@ fun main() {
     }
 
     while (true) {
-        println("""
+        println(
+            """
             Меню:
             1 - Учить слова
             2 - Статистика
             0 - Выход
-        """.trimIndent())
+        """.trimIndent()
+        )
+
         when (readlnOrNull()?.trim()) {
             "1" -> println("Выбран пункт 1 - Учить слова")
-            "2" -> println("Выбран пункт 2 - Статистика")
+            "2" -> {
+                val numberOfWords = dictionary.size
+                val numberOfLearnedWords = dictionary
+                    .filter { it.correctAnswersCount >= MIN_CORRECT_ANSWERS_COUNT }.size
+                val percentage = numberOfLearnedWords * 100 / numberOfWords
+
+                println("Выучено $numberOfLearnedWords из $numberOfWords слов | $percentage%")
+            }
+
             "0" -> break
             else -> println("Неверный ввод, введите 1, 2 или 0")
         }
