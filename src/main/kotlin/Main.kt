186e406be77b4bd11e941bbee1fb1d96e430d199
paul_ <pavel.mordukhai@gmail.com>
2024-09-03 20:59:56 +0700

    KTB-07 функционал изучения слов: варианты ответа

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,6 +3,7 @@ package org.example
 import java.io.File
 
 const val MIN_CORRECT_ANSWERS_COUNT = 3
+const val NUMBER_OF_ANSWER_CHOICES = 4
 
 data class Word(
     val original: String,
@@ -13,14 +14,14 @@ data class Word(
 fun main() {
     val dictionary: MutableList<Word> = mutableListOf()
 
-    val wordsFile: File = File("words.txt")
+    val wordsFile = File("words.txt")
     wordsFile.createNewFile()
-    wordsFile.writeText("hello|привет|4\n")
-    wordsFile.appendText("dog|собака|3\n")
-    wordsFile.appendText("cat|кошка|3\n")
-    wordsFile.appendText("green|зелёный|0\n")
-    wordsFile.appendText("read|читать|4\n")
-    wordsFile.appendText("thank you|спасибо|")
+    wordsFile.writeText("hello|привет|1\n")
+    wordsFile.appendText("dog|собака|1\n")
+    wordsFile.appendText("cat|кошка|1\n")
+    wordsFile.appendText("green|зелёный|1\n")
+    wordsFile.appendText("read|читать|1\n")
+    wordsFile.appendText("thank you|спасибо|1")
 
     val lines: List<String> = wordsFile.readLines()
     for (line in lines) {
@@ -32,6 +33,7 @@ fun main() {
     while (true) {
         println(
             """
+                
             Меню:
             1 - Учить слова
             2 - Статистика
@@ -40,7 +42,40 @@ fun main() {
         )
 
         when (readlnOrNull()?.trim()) {
-            "1" -> println("Выбран пункт 1 - Учить слова")
+            "1" -> {
+                println("\nВведите 0 для выхода в главное меню\n")
+                while (true) {
+                    val unlearnedWords = dictionary
+                        .filter { it.correctAnswersCount < MIN_CORRECT_ANSWERS_COUNT }.shuffled()
+
+                    if (unlearnedWords.isEmpty()) {
+                        println("Вы выучили все слова")
+                        break
+                    }
+                    val unlearnedWordsCount = unlearnedWords.size
+
+                    val answers = if (unlearnedWordsCount <= NUMBER_OF_ANSWER_CHOICES)
+                        unlearnedWords
+                    else
+                        unlearnedWords.take(NUMBER_OF_ANSWER_CHOICES)
+
+                    val englishWord = answers.random().original
+                    println(englishWord)
+
+                    for (i in answers.indices) {
+                        print("${i + 1} - ${answers[i].translate}")
+                        if (i < (answers.size - 1)) print(", ") else println()
+                    }
+
+                    print("Ваш ответ: ")
+                    if (readlnOrNull()?.trim() == "0") break
+                    else {
+                        println()
+                        continue
+                    }
+                }
+            }
+
             "2" -> {
                 val numberOfWords = dictionary.size
                 val numberOfLearnedWords = dictionary
